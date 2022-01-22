818a992e4bba58faf060ac8a9417c4df2fb45439
Salaudeen O. Abdulrasaq <82021949+Sirlawdin@users.noreply.github.com>
2022-01-22 11:18:46 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,12 @@
-def buildApp() {
-    echo 'building the application...'
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+}
+def buildImage() {
+    echo "building the doker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){ sh 'docker build -t sirlawdin/my-devops-repo:jma-2.0 .' 
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push sirlawdin/my-devops-repo:jma-2.0'
 }
 
 def testApp() {
@@ -12,4 +19,4 @@ def deployApp() {
 }
 
 
-return this
+return this 
